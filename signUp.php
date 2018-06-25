49e5e0cbe0bc42b5cd781a0816b5311365384f33
md-aqil <aqilali381@gmail.com>
2018-06-25 16:47:12 -0700

    added

--- signUp.php
+++ signUp.php
@@ -5,7 +5,9 @@
 		background: #eaeef0;
 	}
 </style>
+<div class="login-wrap">
 
+<canvas class="particle" id="myCanvas" width="1200" height="600"></canvas>
 
 <div class="ui container user-login-box-container">
 	<div class="user-login-box">
@@ -20,60 +22,136 @@
 		  	  </a>
 
 		  	  <div class="ui right dropdown item">
-		  	    <span class="text-muted" style="margin-right: 10px">If you already have an Account</span> <button class="ui primary basic own round button">Login</button>
+		  	    <span class="text-muted" style="margin-right: 10px">If you already have an Account</span> <a href="login.php" class="ui primary basic own round button">Login</a>
 		  	   
 		  	  </div>
 		  	</div>
 		 
 		  		
-		  		<div class="ui tab active" data-tab="tab-name">
+		  		<div class="ui tab active" data-tab="select-option">
 		  			
-		  		
-		  		</div>
-
-		  	
-<div style="padding: 50px 50px;">
-	<h2 class="ui header text-center mb0">Get Started Now</h2>
-			  				
-			 <p class="text-center text-muted">
-			 	 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae veritatis
-			 </p>
-			 <h1>&nbsp;</h1>
-	  	 <div class="ui grid">
+					<div style="padding: 50px 50px;">
+						<h2 class="ui header text-center mb0">Get Started Now</h2>
+								  				
+								 <p class="text-center text-muted">
+								 	 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae veritatis
+								 </p>
+								 <h1>&nbsp;</h1>
+						  	 <div class="ui grid">
 
 
-	  	      <div class="eight wide column">
-	  	      	<input type="radio" class="img-checkbox" id="one-img" name="radio">
-	  	      	<label for="one-img" class="select-img">
-	  	      		<img src="img/Seller-signup.svg" alt="">
-	  	      	</label>
-	  	      </div>
-	  	      <div class="eight wide column">
-	  	      	<input type="radio" id="two-img" class="img-checkbox" name="radio">
-      	  	      	<label for="two-img" class="select-img">
-      	  	      		<img src="img/Seller-signup.svg" alt="">
-      	  	      	</label>
-	  	      </div>
+						  	      <div class="eight wide column">
+						  	      	<input type="radio" class="img-checkbox" id="one-img" name="radio">
+						  	      	<label for="one-img" class="select-img">
+						  	      		<img src="img/Seller-signup.svg" alt="">
+						  	      	</label>
+						  	      </div>
+						  	      <div class="eight wide column">
+						  	      	<input type="radio" id="two-img" class="img-checkbox" name="radio">
+					      	  	      	<label for="two-img" class="select-img">
+					      	  	      		<img src="img/Seller-signup.svg" alt="">
+					      	  	      	</label>
+						  	      </div>
 
 
-	  	    </div>
+						  	    </div>
 
-	  	    <div class="clearing mt60">
-	  	    	
-	  	    	<button class="ui basic left floated button">
-				
-				  BACK
-				</button>
-	  	    	<button class="ui primary right floated button">NEXT</button>
-	  	    	  
-	  	    	
-	  	    </div>
-</div>
+				  	      	    <div class="clearing mt100">
+				  	      	    	
+				  	      	    	<button class="ui basic left floated button">
+				  	    			
+				  	    			  BACK
+				  	    			</button>
+				  	      	    	<button class="ui primary right floated button signup-seller">NEXT</button>
+				  	      	    	  
+				  	      	    	
+				  	      	    </div>	  	
+
+					</div>
+				</div>
+
+				<div class="ui tab transition fade left in" data-tab="tab-name">
+				 	<form class="ui form bg-gray-form">
+				 	  <div class="field">
+				 	    <label>Your Name</label>
+				 	    <input type="text" name="first-name" placeholder="Your Name">
+				 	  </div>
+				 	  <div class="field">
+				 	    <label>Your Email</label>
+				 	    <input type="text" name="last-name" placeholder="">
+				 	  </div>
+				 	   <div class="field">
+				 	    <label>Mobile</label>
+				 	    <input type="text" name="last-name" placeholder="">
+				 	  </div>
+				 	    <div class="field">
+				 	    <label>Password</label>
+				 	    <input type="text" name="last-name" placeholder="">
+				 	  </div>
+				 	   <div class="field">
+				 	    <label>Confirm Password</label>
+				 	    <input type="text" name="last-name" placeholder="">
+				 	  </div>
+				 	  <div class="field">
+				 	    <div class="ui checkbox">
+				 	      <input type="checkbox" tabindex="0" class="hidden">
+				 	      <label>I agree to the Terms and Conditions</label>
+				 	    </div>
+				 	  </div>
+				 	</form>
+
+		 	  	    <div class="clearing" style="padding-top:40px;">
+		 	  	    	
+		 	  	    	<button class="ui basic left floated button">
+		 				
+		 				  BACK
+		 				</button>
+		 	  	    	<button class="ui primary right floated button signup-seller">SUBMIT</button>
+		 	  	    	  
+		 	  	    	
+		 	  	    </div>	  	
+				</div>
+
+
+
+
+				  	   
 		  
 		  </div>
 
-			  <div class="eight wide column">
-			  	
+
+
+
+
+			  <div class="eight wide column" style="background: #fbfbfb;">
+			  		<div id="example4" class="slider-pro">
+			  			<div class="sp-slides">
+			  			
+			  			
+			  				<div class="sp-slide text-center">
+			  					<div class="intro-box">
+			  						<h3 class="header ui fw400 mt30">What is lorem ipsum?</h3>
+			  						<img src="img/Seller-signup.svg" alt="">
+			  						<p class="text-muted mt50">
+			  							Lorem ipsum dolor sit amet, consectetur  adipisicing elit.<br> Natus, necessitatibus?
+			  						</p>
+			  					</div>
+			  					
+			  				</div>
+							<div class="sp-slide text-center">
+			  					<div class="intro-box">
+			  						<h3 class="header ui fw400 mt30">What is lorem ipsum?</h3>
+			  						<img src="img/Seller-signup.svg" alt="">
+			  						<p class="text-muted mt50">
+			  							Lorem ipsum dolor sit amet, consectetur  adipisicing elit.<br> Natus, necessitatibus?
+			  						</p>
+			  					</div>
+			  					
+			  				</div>
+
+			  			
+			  			</div>
+			  	    </div>
 			  
 			  </div>
 			 
@@ -84,6 +162,6 @@
 	</div>
 </div>
 
-
+</div>
 
 <?php include 'includes/footer.php'; ?>
